6ede8c16b558d644c9c8b4bf88a0dc1d0c6c36a7
sixpoint9 <133214364+sixpoint9@users.noreply.github.com>
2025-03-28 14:32:07 +0100

    Reworked Navbar, added logo as svg, configured webpack to import svg as component

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,31 +1,84 @@
+"use client";
 import Link from "next/link";
-import { Button } from "./button";
+import Logo from "@/public/Kairos-Logo.svg";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function Navbar() {
+  const [teamsOpen, setTeamsOpen] = useState(false);
+  const [resultsOpen, setResultsOpen] = useState(false);
+
   return (
-    <nav className="border-b">
-      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        <div className="flex items-center space-x-1">
-          <Link href="/" className="font-bold text-xl">
+    <nav className="border-b w-full">
+      <div className="px-10 py-3 flex items-center justify-between w-full pr-40">
+        <div className="flex items-center gap-3">
+          <Logo width={120} height={52} className="h-8 w-auto" />
+          <Link href="/" className="font-semibold text-lg">
             Debate Forms
           </Link>
         </div>
-        <div className="flex items-center space-x-4">
-          <Link href="/">
-            <Button variant="ghost">Evaluar</Button>
+        <div className="flex items-center gap-6">
+          <Link
+            href="/"
+            className="text-sm font-medium hover:text-accent-foreground"
+          >
+            Evaluar
           </Link>
-          <Link href="/admin">
-            <Button variant="ghost">Admin</Button>
+          <Link
+            href="/admin"
+            className="text-sm font-medium hover:text-accent-foreground"
+          >
+            Admin
           </Link>
-          <Link href="/admin/teams-create">
-            <Button variant="outline">Crear Equipo</Button>
-          </Link>
-          <Link href="/admin/teams-view">
-            <Button variant="outline">Ver Equipos</Button>
-          </Link>
-          <Link href="/admin/results">
-            <Button variant="outline">Ver Resultados</Button>
-          </Link>
+          <DropdownMenu open={teamsOpen} onOpenChange={setTeamsOpen}>
+            <DropdownMenuTrigger
+              className="text-sm font-medium hover:text-accent-foreground "
+              onMouseEnter={() => setTeamsOpen(true)}
+              onMouseLeave={() => setTeamsOpen(false)}
+            >
+              Equipos
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="-mt-0.5"
+              onMouseEnter={() => setTeamsOpen(true)}
+              onMouseLeave={() => setTeamsOpen(false)}
+              align="start"
+            >
+              <DropdownMenuItem asChild>
+                <Link href="/admin/teams-create">Crear Equipo</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/admin/teams-view">Ver Equipos</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu open={resultsOpen} onOpenChange={setResultsOpen}>
+            <DropdownMenuTrigger
+              className="text-sm font-medium hover:text-accent-foreground"
+              onMouseEnter={() => setResultsOpen(true)}
+              onMouseLeave={() => setResultsOpen(false)}
+            >
+              Resultados
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="-mt-0.5"
+              onMouseEnter={() => setResultsOpen(true)}
+              onMouseLeave={() => setResultsOpen(false)}
+              align="start"
+            >
+              <DropdownMenuItem asChild>
+                <Link href="/admin/results">Ver Resultados</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/admin/results/best">Ver Mejores Oradores</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </nav>
